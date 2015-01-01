0d1f75e60b4b71a025198880cb14101023863182
Eslem Alzate <eslemalzate1@EslemAzates-Mac.local>
2014-12-31 21:07:10 -0800

    Initial Commit

---
+++ Classes&Objects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Classes&Objects
+//
+//  Created by Eslem Alzate on 31/12/14.
+//  Copyright (c) 2014 Alzate Zabala. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
